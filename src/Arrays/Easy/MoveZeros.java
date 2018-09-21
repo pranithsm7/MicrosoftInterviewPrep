6b6a216c3f8d4215b8dd5213667086b36478fc5b
Pranith Srinivas Mididoddi <pranithsrinivasmididoddi@Praniths-MBP.attlocal.net>
2018-09-20 20:22:00 -0500

    Move Zeros to corner

--- src/Arrays/Easy/MoveZeros.java
+++ src/Arrays/Easy/MoveZeros.java
@@ -1,4 +1,37 @@
 package Arrays.Easy;
 
+import java.util.Arrays;
+
 public class MoveZeros {
+
+    //2,0,1,0,4,5,0,9
+
+    public void moveZerosToRight(int[] array){
+
+        int initialPointer=0,movingPointer=0;
+        int arrayLength=array.length;
+
+        while(movingPointer<arrayLength){
+
+            if(array[movingPointer]==0){
+                movingPointer++;
+            }else{
+                array[initialPointer]=array[movingPointer];
+                initialPointer++;
+                movingPointer++;
+            }
+        }
+
+        while(initialPointer<arrayLength){
+            array[initialPointer]=0;
+            initialPointer++;
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void main(String[] args){
+
+        new MoveZeros().moveZerosToRight(new int[]{2,0,1,0,4,5,0,9});
+    }
 }
