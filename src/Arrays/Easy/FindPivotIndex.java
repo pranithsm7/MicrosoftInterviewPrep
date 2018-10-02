e51a04d6509d90da6ffdb169e35c283d88696679
Pranith Srinivas Mididoddi <pranithsrinivasmididoddi@Praniths-MBP.attlocal.net>
2018-10-01 19:07:54 -0500

    Find pivot index

--- src/Arrays/Easy/FindPivotIndex.java
+++ src/Arrays/Easy/FindPivotIndex.java
@@ -1,4 +1,26 @@
 package Arrays.Easy;
 
 public class FindPivotIndex {
+
+
+    public static void main(String[] args){
+
+        int rightValue=0;
+        int leftValue=0;
+        int[] array={1,2,3,4,3,3};
+
+        for(int i=1;i<array.length;i++){
+            rightValue+=array[i];
+        }
+
+        for(int i=1;i<array.length-1;i++){
+            leftValue+=array[i-1];
+            rightValue-=array[i];
+
+            if(leftValue==rightValue){
+                System.out.println(i);
+            }
+        }
+
+    }
 }
