660715cafadb3ccb81254bf018c9aac0d8a935fe
Pranith Srinivas Mididoddi <pranithsrinivasmididoddi@Praniths-MBP.attlocal.net>
2018-10-08 21:03:38 -0500

    Sort colors

--- src/Arrays/Medium/SortColors.java
+++ src/Arrays/Medium/SortColors.java
@@ -1,4 +1,41 @@
 package Arrays.Medium;
 
+import java.util.Arrays;
+
 public class SortColors {
+
+    public void sortColorsInOccurrance(int[] array){
+
+        int[] colors=new int[3];
+
+        for(int i=0;i<array.length;i++){
+            colors[array[i]]++;
+        }
+
+        for(int i=0;i<colors.length;i++){
+            System.out.println(colors[i]);
+        }
+
+        int appender=0;
+        int i=0;
+
+        while(appender<=2){
+
+            if(colors[appender]!=0){
+                array[i++]=appender;
+                colors[appender]=colors[appender]-1;
+            }else{
+                appender++;
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+
+
+    public static void main(String[] args){
+
+        int[] array={0,2,1,0,1,0,2};
+        new SortColors().sortColorsInOccurrance(array);
+    }
 }
