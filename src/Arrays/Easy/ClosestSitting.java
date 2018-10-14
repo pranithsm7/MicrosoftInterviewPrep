6ce4f4b0ac04082f492591e3015ff7f5d1401ecc
Pranith Srinivas Mididoddi <pranithsrinivasmididoddi@praniths-mbp.attlocal.net>
2018-10-14 09:15:37 -0500

    Revision

---
+++ src/Arrays/Easy/ClosestSitting.java
@@ -0,0 +1,15 @@
+package Arrays.Easy;
+
+import java.util.Arrays;
+
+public class ClosestSitting {
+
+    public static void main(String[] args){
+
+        int[] array=new int[10];
+
+        Arrays.fill(array,10);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
---
+++ src/Revise/ProductOfAllNumbers.java
@@ -0,0 +1,12 @@
+package Revise;
+
+public class ProductOfAllNumbers {
+
+    public int[] productOfAllNumbers(int[] array){
+
+        int[] array1=new int[array.length];
+
+        return null;
+
+    }
+}
