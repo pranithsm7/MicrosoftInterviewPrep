694c55053d6fdf4d302e7ee1178a064d9e996524
Pranith Srinivas Mididoddi <pranithsrinivasmididoddi@praniths-mbp.attlocal.net>
2018-11-08 00:58:47 -0600

    Change

---
+++ src/Stack/StackUsingQueues.java
@@ -0,0 +1,57 @@
+package Stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsingQueues {
+
+    private Queue<Integer> queue;
+    private Queue<Integer> tempqueue;
+
+    public StackUsingQueues(){
+
+        queue=new LinkedList<>();
+        tempqueue=new LinkedList<>();
+    }
+
+    public void push(int number){
+
+        if(queue.isEmpty()){
+            queue.offer(number);
+        }else{
+
+            while (queue.size()>0){
+                tempqueue.offer(queue.remove());
+            }
+
+            queue.offer(number);
+
+            while(tempqueue.size()>0){
+                queue.offer(tempqueue.remove());
+            }
+        }
+    }
+
+    public int pop(){
+        return queue.peek();
+    }
+
+    public boolean isEmpty(){
+        return queue.size()==0;
+    }
+
+    public int topOfStack(){
+        return queue.peek();
+    }
+
+
+    public static void main(String[] args){
+        StackUsingQueues stackUsingQueues=new StackUsingQueues();
+        stackUsingQueues.push(5);
+        stackUsingQueues.push(4);
+        stackUsingQueues.push(3);
+        stackUsingQueues.push(2);
+
+        System.out.println(stackUsingQueues);
+    }
+}
