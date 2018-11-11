36261a4f6a1a6174d9f82936ba03732fe035b176
Pranith Srinivas Mididoddi <pranithsrinivasmididoddi@praniths-mbp.attlocal.net>
2018-11-10 20:03:10 -0600

    HashTable implementation

--- src/HashTable/HashTableImplementation/HashEntry.java
+++ src/HashTable/HashTableImplementation/HashEntry.java
@@ -2,9 +2,9 @@ package HashTable.HashTableImplementation;
 
 public class HashEntry {
 
-    private String key;
-    private String value;
-    private HashEntry next;
+    public String key;
+    public String value;
+    public HashEntry next;
 
     public HashEntry(String key, String value){
 
