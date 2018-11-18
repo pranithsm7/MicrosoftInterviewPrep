118c56f3d96cf49744d042b697268d34a1338e9f
Pranith Srinivas Mididoddi <pranithsrinivasmididoddi@praniths-mbp.attlocal.net>
2018-11-18 09:58:57 -0600

    First Unique Character

--- src/AmazonPrep/ArraysAndStrings.java
+++ src/AmazonPrep/ArraysAndStrings.java
@@ -1,4 +1,32 @@
 package AmazonPrep;
 
 public class ArraysAndStrings {
+
+    public int firstUniqueChar(String s){
+
+        if(s.length()==0 || s==null){
+            return -1;
+        }
+
+        int[] array=new int[26];
+
+        for(char c : s.toCharArray()){
+            array[c-'a']++;
+        }
+
+        int index=0;
+
+        for(int i=0;i<s.length();i++){
+            if(array[s.charAt(i)-'a']==1){
+                index=i;
+                return index;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(new ArraysAndStrings().firstUniqueChar("loveleetcode"));
+    }
 }
